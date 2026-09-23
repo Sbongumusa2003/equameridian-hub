28de5d2fe32f0412844890ef4cd73dda2f8c33e3
Sbongumusa Mbatha <u22710362@up.ac.za>
2026-09-23 18:07:22 +0200

    Landing page fixed

--- src/app/layout/public-layout/public-layout.component.ts
+++ src/app/layout/public-layout/public-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { AuthService } from '../../core/services/auth.service';
@@ -12,6 +12,7 @@ import { AuthService } from '../../core/services/auth.service';
 export class PublicLayoutComponent {
   currentYear = new Date().getFullYear();
   isHome = true;
+  menuOpen = false;
 
   constructor(public auth: AuthService, private router: Router) {
     this.isHome = this.router.url === '/' || this.router.url === '';
@@ -19,6 +20,22 @@ export class PublicLayoutComponent {
       .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
       .subscribe(e => {
         this.isHome = e.urlAfterRedirects === '/' || e.urlAfterRedirects === '';
+        this.closeMenu();
       });
+  }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    if (typeof window !== 'undefined' && window.innerWidth > 768) {
+      this.menuOpen = false;
+    }
   }
 }
